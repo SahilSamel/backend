a9c46bb4481e0c5809e6e87a0a88bcfd89f34329
AlmightyAmogh <amogh2020@gmail.com>
2022-07-26 12:45:00 +0530

    Frontend First commit basic

---
+++ Frontend/codetantra_scheduler/src/App.js
@@ -0,0 +1,16 @@
+
+import { Button } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is apps
+      <Button variant="text">Text</Button>
+<Button variant="contained">Contained</Button>
+<Button variant="outlined">Outlined</Button>
+    </div>
+  );
+}
+
+export default App;
